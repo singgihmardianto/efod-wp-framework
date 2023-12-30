1ce24e5fd20464e4bff478f44477622186e26125
Singgih Mardianto <singgihmardianto@gmail.com>
2023-12-30 13:53:21 +0700

    build(add dependencies): add bootstrap; add fontawesome; change build & deploy script

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -5,4 +5,4 @@ PLUGIN_DEV_DIR="./"
 
 # clean all release folders
 rm -rf "$PLUGIN_RELEASE_DIR"
-rsync -av --exclude '.*' --exclude '.git' --exclude 'commands' --exclude 'assets' --exclude 'node_modules' --exclude 'vendor' $PLUGIN_DEV_DIR $PLUGIN_RELEASE_DIR
+rsync -av --exclude '.*' --exclude 'styles' --exclude 'scripts' --exclude '.git' --exclude 'commands' --exclude 'node_modules' --exclude 'vendor' $PLUGIN_DEV_DIR $PLUGIN_RELEASE_DIR
