4173f4c758a12a5ed2e972c23cf5c4598f771c05
Singgih Mardianto <singgihmardianto@gmail.com>
2024-01-14 09:58:37 +0700

    fix(template view): fix bad logic template view

--- includes/templates/widgets/loop-job-vacancy.php
+++ includes/templates/widgets/loop-job-vacancy.php
@@ -7,159 +7,158 @@
 
 // Grouped some types into another array.
 $is_list_layout    = array_intersect_key( $responsive_layout_type, array_flip( array( 'grid-3', 'grid-4', 'masonry-3', 'masonry-4' ) ) );
-$joined_list_value = join( ' ', $is_list_layout );
-if ( $q && $q->have_posts() ) { ?>
-	<?php if ( $responsive_layout_type && ! empty( $joined_list_value ) ) : ?>
-		<div class="ef-loop ef-paginate_<?php echo esc_html( $pagination_type ); ?> <?php echo esc_html( $joined_list_value ); ?>">
-			<div class="ef-loop-container ef-catalog <?php echo esc_html( $layout_type ); ?>">
-				<?php
-				while ( $q->have_posts() ) :
-					$q->the_post();
-					?>
-					<?php $ef_post_id = $q->post->ID; ?>
-					<div class="ef-widget__catalog">
-						<div class="ef-widget__wrapper">
-							<div class="ef-post-thumbnail">
-								<?php efod_the_thumbnail(); ?>
+$joined_list_value = join( ' ', $is_list_layout ); ?>
+
+<?php if ( $q && $responsive_layout_type && ! empty( $joined_list_value ) ) : ?>
+	<div class="ef-loop ef-paginate_<?php echo esc_html( $pagination_type ); ?> <?php echo esc_html( $joined_list_value ); ?>">
+		<div class="ef-loop-container ef-catalog <?php echo esc_html( $layout_type ); ?>">
+			<?php
+			while ( $q->have_posts() ) :
+				$q->the_post();
+				?>
+				<?php $ef_post_id = $q->post->ID; ?>
+				<div class="ef-widget__catalog">
+					<div class="ef-widget__wrapper">
+						<div class="ef-post-thumbnail">
+							<?php efod_the_thumbnail(); ?>
+						</div>
+						<div class="ef-post-body">
+							<h3 class="ef-title"> 
+								<?php the_title(); ?> <?php efod_posted_on(); ?>
+							</h3>
+							<div class="ef-description">  
+								<?php the_content(); ?> 
 							</div>
-							<div class="ef-post-body">
-								<h3 class="ef-title"> 
-									<?php the_title(); ?> <?php efod_posted_on(); ?>
-								</h3>
-								<div class="ef-description">  
-									<?php the_content(); ?> 
-								</div>
-								<div class="ef-post-meta">
-									<?php efod_custom_posts_meta( 'catalog' ); ?>
-								</div>
+							<div class="ef-post-meta">
+								<?php efod_custom_posts_meta( 'catalog' ); ?>
 							</div>
 						</div>
-					</div> <!-- .ef-widget__catalog -->
-				<?php endwhile; ?>
-			</div> <!-- .ef-loop-container.ef-catalog -->
-			<?php
-			efod_pagination_links(
-				array(
-					'q'             => $q,
-					'type'          => $pagination_type,
-					'post_type'     => 'catalogs',
-					'post_per_page' => $data_counts,
-				)
-			);
-			?>
-		</div> <!-- .ef-loop.ef-paginate -->
-	<?php endif; ?>
-	<?php if ( $responsive_layout_type && ! empty( $responsive_layout_type['tab'] ) ) : ?>
-		<div class="ef-loop ef-tabs <?php echo esc_html( $responsive_layout_type['tab'] ); ?>">
-			<ul class="nav nav-tabs" id="efCatalogTab" role="tablist">
-				<?php
-				foreach ( $terms as $i => $tax_term ) :
-					$cur_term = $tax_term['term'];
-					?>
-					<li class="nav-item" role="presentation">
-						<button class="nav-link <?php echo esc_html( 0 === $i ? 'active' : '' ); ?>" id="<?php echo esc_html( $cur_term->slug ); ?>-tab" data-bs-toggle="tab" data-bs-target="#<?php echo esc_html( $cur_term->slug ); ?>-tab-pane" type="button" role="tab"><?php echo esc_html( $cur_term->name ); ?></button>
-					</li>	
-				<?php endforeach; ?>
-			</ul> <!--.nav.nav-tabs -->
-			<div class="tab-content" id="efCatalogTabContent">
-				<?php
-				foreach ( $terms as $i => $tax_term ) :
-					$cur_term = $tax_term['term'];
-					$q        = $tax_term['q'];
-					?>
-					<div class="tab-pane fade <?php echo esc_html( 0 === $i ? 'show active' : 'fade' ); ?>" id="<?php echo esc_html( $cur_term->slug ); ?>-tab-pane" role="tabpanel">
-						<ul >
-							<?php
-							while ( $q->have_posts() ) :
-								$q->the_post();
-								?>
-								<li>
-									<div class="ef-widget__catalog">
-										<div class="ef-widget__wrapper">
-											<div class="ef-post-thumbnail">
-												<?php efod_the_thumbnail(); ?>
-											</div>
-											<div class="ef-post-body">
-												<h3 class="ef-title"> 
-													<?php the_title(); ?>
-												</h3>
-												<div class="ef-description"> 
-													<?php the_content(); ?> 
-												</div>
-												<?php if ( '' !== $cta_url['url'] ) : ?>
-													<div class="ef-post-meta">
-														<a href="<?php echo esc_url( $cta_url['url'] ); ?>" target="<?php echo esc_html( true === $cta_url['is_external'] ? '_blank' : '' ); ?>">
-															<?php echo esc_html( '' !== $cta_title ? $cta_title : __( 'Apply Now', 'efod-framework' ) ); ?>
-														</a>
-													</div>
-												<?php endif; ?>
-											</div>
-										</div>
-									</div> <!-- .ef-widget__catalog -->
-								</li>
-							<?php endwhile; ?>
-						</ul>
 					</div>
-				<?php endforeach; ?>
-			</div> <!--.tab-content -->
-		</div> <!--.ef-loop.ef-tabs -->
-	<?php endif; ?>
-	<?php if ( $responsive_layout_type && ! empty( $responsive_layout_type['accordion'] ) ) : ?>
-		<div class="ef-loop ef-accordion <?php echo esc_html( $responsive_layout_type['accordion'] ); ?>">
-			<div class="accordion accordion-flush" id="catalog_accord">
-				<?php
-				foreach ( $terms as $i => $tax_term ) :
-					$cur_term = $tax_term['term'];
-					$q        = $tax_term['q'];
-					?>
-					<div class="accordion-item">
-						<h2 class="accordion-header">
-							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#ef-accord-<?php echo esc_html( $cur_term->slug ); ?>" aria-expanded="false" aria-controls="ef-accord-<?php echo esc_html( $cur_term->slug ); ?>">
-								<?php echo esc_html( $cur_term->name ); ?>
-							</button>  <!-- .accordion-button -->
-						</h2>  <!-- .accordion-header -->
-						<div id="ef-accord-<?php echo esc_html( $cur_term->slug ); ?>" class="accordion-collapse collapse" data-bs-parent="#catalog_accord">
-							<div class="accordion-body">
-								<ul>
-									<?php
-									while ( $q->have_posts() ) :
-										$q->the_post();
-										?>
-										<li>
-											<div class="ef-widget__catalog">
-												<div class="ef-widget__wrapper">
-													<div class="ef-post-thumbnail">
-														<?php efod_the_thumbnail(); ?>
-													</div>
-													<div class="ef-post-body">
-														<h3 class="ef-title"> 
-															<?php the_title(); ?>
-														</h3>
-														<div class="ef-description">  
-															<?php the_content(); ?> 
-														</div>
-														<?php if ( '' !== $cta_url['url'] ) : ?>
-															<div class="ef-post-meta">
-																<a href="<?php echo esc_url( $cta_url['url'] ); ?>" target="<?php echo esc_html( true === $cta_url['is_external'] ? '_blank' : '' ); ?>">
-																	<?php echo esc_html( '' !== $cta_title ? $cta_title : __( 'Apply Now', 'efod-framework' ) ); ?>
-																</a>
-															</div>
-														<?php endif; ?>
-													</div>
-												</div>
-											</div> <!-- .ef-widget__catalog -->
-										</li>
-									<?php endwhile; ?>
-								</ul>
-							</div>  <!-- .accordion-body -->
-						</div>  <!-- .accordion-collapse.collapse -->
-					</div>  <!-- .accordion-item -->
-				<?php endforeach; ?>
-			</div>  <!-- .accordion.accordion-flush -->
-		</div> <!-- .ef-loop.ef-accordion -->
+				</div> <!-- .ef-widget__catalog -->
+			<?php endwhile; ?>
+		</div> <!-- .ef-loop-container.ef-catalog -->
 		<?php
-	endif;
-} else {
-	efod_get_views( 'content-none' );
-}
-wp_reset_postdata();
+		efod_pagination_links(
+			array(
+				'q'             => $q,
+				'type'          => $pagination_type,
+				'post_type'     => 'catalogs',
+				'post_per_page' => $data_counts,
+			)
+		);
+		?>
+	</div> <!-- .ef-loop.ef-paginate -->
+<?php endif; ?>
+
+<?php if ( $responsive_layout_type && ! empty( $responsive_layout_type['tab'] ) ) : ?>
+	<div class="ef-loop ef-tabs <?php echo esc_html( $responsive_layout_type['tab'] ); ?>">
+		<ul class="nav nav-tabs" id="efCatalogTab" role="tablist">
+			<?php
+			foreach ( $terms as $i => $tax_term ) :
+				$cur_term = $tax_term['term'];
+				?>
+				<li class="nav-item" role="presentation">
+					<button class="nav-link <?php echo esc_html( 0 === $i ? 'active' : '' ); ?>" id="<?php echo esc_html( $cur_term->slug ); ?>-tab" data-bs-toggle="tab" data-bs-target="#<?php echo esc_html( $cur_term->slug ); ?>-tab-pane" type="button" role="tab"><?php echo esc_html( $cur_term->name ); ?></button>
+				</li>	
+			<?php endforeach; ?>
+		</ul> <!--.nav.nav-tabs -->
+		<div class="tab-content" id="efCatalogTabContent">
+			<?php
+			foreach ( $terms as $i => $tax_term ) :
+				$cur_term = $tax_term['term'];
+				$q        = $tax_term['q'];
+				?>
+				<div class="tab-pane fade <?php echo esc_html( 0 === $i ? 'show active' : 'fade' ); ?>" id="<?php echo esc_html( $cur_term->slug ); ?>-tab-pane" role="tabpanel">
+					<ul >
+						<?php
+						while ( $q->have_posts() ) :
+							$q->the_post();
+							?>
+							<li>
+								<div class="ef-widget__catalog">
+									<div class="ef-widget__wrapper">
+										<div class="ef-post-thumbnail">
+											<?php efod_the_thumbnail(); ?>
+										</div>
+										<div class="ef-post-body">
+											<h3 class="ef-title"> 
+												<?php the_title(); ?>
+											</h3>
+											<div class="ef-description"> 
+												<?php the_content(); ?> 
+											</div>
+											<?php if ( '' !== $cta_url['url'] ) : ?>
+												<div class="ef-post-meta">
+													<a href="<?php echo esc_url( $cta_url['url'] ); ?>" target="<?php echo esc_html( true === $cta_url['is_external'] ? '_blank' : '' ); ?>">
+														<?php echo esc_html( '' !== $cta_title ? $cta_title : __( 'Apply Now', 'efod-framework' ) ); ?>
+													</a>
+												</div>
+											<?php endif; ?>
+										</div>
+									</div>
+								</div> <!-- .ef-widget__catalog -->
+							</li>
+						<?php endwhile; ?>
+					</ul>
+				</div>
+			<?php endforeach; ?>
+		</div> <!--.tab-content -->
+	</div> <!--.ef-loop.ef-tabs -->
+<?php endif; ?>
+
+<?php if ( $responsive_layout_type && ! empty( $responsive_layout_type['accordion'] ) ) : ?>
+	<div class="ef-loop ef-accordion <?php echo esc_html( $responsive_layout_type['accordion'] ); ?>">
+		<div class="accordion accordion-flush" id="catalog_accord">
+			<?php
+			foreach ( $terms as $i => $tax_term ) :
+				$cur_term = $tax_term['term'];
+				$q        = $tax_term['q'];
+				?>
+				<div class="accordion-item">
+					<h2 class="accordion-header">
+						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#ef-accord-<?php echo esc_html( $cur_term->slug ); ?>" aria-expanded="false" aria-controls="ef-accord-<?php echo esc_html( $cur_term->slug ); ?>">
+							<?php echo esc_html( $cur_term->name ); ?>
+						</button>  <!-- .accordion-button -->
+					</h2>  <!-- .accordion-header -->
+					<div id="ef-accord-<?php echo esc_html( $cur_term->slug ); ?>" class="accordion-collapse collapse" data-bs-parent="#catalog_accord">
+						<div class="accordion-body">
+							<ul>
+								<?php
+								while ( $q->have_posts() ) :
+									$q->the_post();
+									?>
+									<li>
+										<div class="ef-widget__catalog">
+											<div class="ef-widget__wrapper">
+												<div class="ef-post-thumbnail">
+													<?php efod_the_thumbnail(); ?>
+												</div>
+												<div class="ef-post-body">
+													<h3 class="ef-title"> 
+														<?php the_title(); ?>
+													</h3>
+													<div class="ef-description">  
+														<?php the_content(); ?> 
+													</div>
+													<?php if ( '' !== $cta_url['url'] ) : ?>
+														<div class="ef-post-meta">
+															<a href="<?php echo esc_url( $cta_url['url'] ); ?>" target="<?php echo esc_html( true === $cta_url['is_external'] ? '_blank' : '' ); ?>">
+																<?php echo esc_html( '' !== $cta_title ? $cta_title : __( 'Apply Now', 'efod-framework' ) ); ?>
+															</a>
+														</div>
+													<?php endif; ?>
+												</div>
+											</div>
+										</div> <!-- .ef-widget__catalog -->
+									</li>
+								<?php endwhile; ?>
+							</ul>
+						</div>  <!-- .accordion-body -->
+					</div>  <!-- .accordion-collapse.collapse -->
+				</div>  <!-- .accordion-item -->
+			<?php endforeach; ?>
+		</div>  <!-- .accordion.accordion-flush -->
+	</div> <!-- .ef-loop.ef-accordion -->
+<?php endif; ?>
+
+<?php wp_reset_postdata();
--- includes/templates/widgets/loop-portfolio.php
+++ includes/templates/widgets/loop-portfolio.php
@@ -10,73 +10,71 @@ $is_list_layout    = array_intersect_key( $responsive_layout_type, array_flip( a
 $joined_list_value = join( ' ', $is_list_layout );
 
 $is_icon_layout    = array_intersect_key( $responsive_layout_type, array_flip( array( 'single-icon', 'multi-icon' ) ) );
-$joined_icon_value = join( ' ', $is_icon_layout );
+$joined_icon_value = join( ' ', $is_icon_layout ); ?>
 
-if ( $q && $q->have_posts() ) : ?>
-	<?php if ( ! empty( $title ) ) : ?>
-		<h2 class="ef-title"><?php echo esc_html( $title ); ?></h2>
-	<?php endif; ?>
-	<?php if ( ! empty( $description ) ) : ?>
-		<p class="ef-description"><?php echo esc_html( $description ); ?></p>
-	<?php endif; ?>
-	<?php if ( $responsive_layout_type && ! empty( $joined_list_value ) ) : ?>
-		<div class="ef-loop ef-paginate_<?php echo esc_html( $pagination_type ); ?> <?php echo esc_html( $joined_list_value ); ?>">
-			<div class="ef-loop-container ef-portfolio <?php echo esc_html( $layout_type ); ?>">
-				<?php
-				while ( $q->have_posts() ) :
-					$q->the_post();
-					?>
-					<?php $ef_post_id = $q->post->ID; ?>
-					<div class="ef-widget__portfolio">
-						<div class="ef-widget__wrapper">
-							<div class="ef-post-thumbnail">
-								<?php efod_the_thumbnail( array( 'custom_link_field' => 'efod_portfolio_project_link' ) ); ?>
-							</div> <!-- .ef-post-thumbnail -->
-							<div class="ef-post-body">
-								<h3 class="ef-title"> 
-									<?php the_title(); ?> <?php efod_posted_on(); ?>
-								</h3>
-								<div class="ef-description"> 
-									<?php the_excerpt(); ?> 
-								</div>
-								<div class="ef-post-meta">
-									<?php efod_custom_posts_meta( 'portfolio' ); ?>
-								</div>
-							</div> <!-- .ef-post-body -->
-						</div> <!-- .ef-widget__wrapper -->
-					</div> <!-- .ef-widget__portfolio -->
-				<?php endwhile; ?>
-			</div> <!-- .ef-loop-container.ef-portfolio -->
+<?php if ( ! empty( $title ) ) : ?>
+	<h2 class="ef-title"><?php echo esc_html( $title ); ?></h2>
+<?php endif; ?>
+<?php if ( ! empty( $description ) ) : ?>
+	<p class="ef-description"><?php echo esc_html( $description ); ?></p>
+<?php endif; ?>
+
+<?php if ( $q && $responsive_layout_type && ! empty( $joined_list_value ) ) : ?>
+	<div class="ef-loop ef-paginate_<?php echo esc_html( $pagination_type ); ?> <?php echo esc_html( $joined_list_value ); ?>">
+		<div class="ef-loop-container ef-portfolio <?php echo esc_html( $layout_type ); ?>">
 			<?php
-			efod_pagination_links(
-				array(
-					'q'             => $q,
-					'type'          => $pagination_type,
-					'post_type'     => 'portfolios',
-					'post_per_page' => $data_counts,
-				)
-			);
-			?>
-		</div>
-	<?php endif; ?>
-	<?php if ( $responsive_layout_type && ! empty( $joined_icon_value ) ) : ?>
-		<div class="ef-loop ef-portfolio-icon <?php echo esc_html( $joined_icon_value ); ?>">
-			<ul>
+			while ( $q->have_posts() ) :
+				$q->the_post();
+				?>
+				<?php $ef_post_id = $q->post->ID; ?>
+				<div class="ef-widget__portfolio">
+					<div class="ef-widget__wrapper">
+						<div class="ef-post-thumbnail">
+							<?php efod_the_thumbnail( array( 'custom_link_field' => 'efod_portfolio_project_link' ) ); ?>
+						</div> <!-- .ef-post-thumbnail -->
+						<div class="ef-post-body">
+							<h3 class="ef-title"> 
+								<?php the_title(); ?> <?php efod_posted_on(); ?>
+							</h3>
+							<div class="ef-description"> 
+								<?php the_excerpt(); ?> 
+							</div>
+							<div class="ef-post-meta">
+								<?php efod_custom_posts_meta( 'portfolio' ); ?>
+							</div>
+						</div> <!-- .ef-post-body -->
+					</div> <!-- .ef-widget__wrapper -->
+				</div> <!-- .ef-widget__portfolio -->
+			<?php endwhile; ?>
+		</div> <!-- .ef-loop-container.ef-portfolio -->
+		<?php
+		efod_pagination_links(
+			array(
+				'q'             => $q,
+				'type'          => $pagination_type,
+				'post_type'     => 'portfolios',
+				'post_per_page' => $data_counts,
+			)
+		);
+		?>
+	</div>
+<?php endif; ?>
+
+<?php if ( $q && $responsive_layout_type && ! empty( $joined_icon_value ) ) : ?>
+	<div class="ef-loop ef-portfolio-icon <?php echo esc_html( $joined_icon_value ); ?>">
+		<ul>
+			<?php
+			while ( $q->have_posts() ) :
+				$q->the_post();
+				?>
 				<?php
-				while ( $q->have_posts() ) :
-					$q->the_post();
-					?>
-					<?php
-					$image_id = get_post_meta( $q->post->ID, 'efod_portfolio_client_icon', true );
-						$url  = wp_get_attachment_image_url( $image_id );
-					?>
-					<li><img src="<?php echo esc_html( $url ); ?>" alt="<?php the_title(); ?>"/></li>
-				<?php endwhile; ?>
-			</ul>
-		</div>
-	<?php endif; ?>
-	<?php
-else :
-	efod_get_views( 'content-none' );
-endif;
-wp_reset_postdata();
+				$image_id = get_post_meta( $q->post->ID, 'efod_portfolio_client_icon', true );
+					$url  = wp_get_attachment_image_url( $image_id );
+				?>
+				<li><img src="<?php echo esc_html( $url ); ?>" alt="<?php the_title(); ?>"/></li>
+			<?php endwhile; ?>
+		</ul>
+	</div>
+<?php endif; ?>
+
+<?php wp_reset_postdata();
